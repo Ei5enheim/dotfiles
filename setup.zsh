4c8efd8a696e709b5316b313c091636f7663ae9f
Ei5enheim <rajesh@cs.unc.edu>
2024-02-15 18:41:59 -0500

    Update setup.zsh

--- setup.zsh
+++ setup.zsh
@@ -3,6 +3,25 @@
 export PS1="%F{green} %~> %f"
 export PATH=$PATH:/opt/homebrew/bin
 
+# install fzf binary
+brew install fzf
+
+# enable auto-complete
+$(brew --prefix)/opt/fzf/install
+
+# install fd
+# alternative to fd which is faster.
+brew install fd
+
+# install ripgrep
+brew install ripgrep
+
+# install bat for syntax highlighting.
+brew install bat
+
+# install delta
+brew install git-delta
+
 # The absolute, canonical ( no ".." ) path to this script
 curr_dir=$(cd -P -- "$(dirname -- "$0")" && echo "$(pwd -P)")
 
