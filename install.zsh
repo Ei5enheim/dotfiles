93a2efb90f1c8ff8d2cfe89eba738a39cdb7d4f2
Ei5enheim <rajesh@cs.unc.edu>
2024-02-15 19:26:54 -0500

    move fzf install commands to install.sh

--- install.zsh
+++ install.zsh
@@ -2,5 +2,23 @@
 
 # The absolute, canonical ( no ".." ) path to this script
 curr_dir=$(cd -P -- "$(dirname -- "$0")" && echo "$(pwd -P)")
-
 echo "source ${curr_dir}/setup.zsh" >> ~/.zshrc
+
+# install fzf binary
+brew install fzf
+
+# enable auto-complete
+$(brew --prefix)/opt/fzf/install
+
+# install fd
+# alternative to fd which is faster.
+brew install fd
+
+# install ripgrep
+brew install ripgrep
+
+# install bat for syntax highlighting.
+brew install bat
+
+# install delta
+brew install git-delta
