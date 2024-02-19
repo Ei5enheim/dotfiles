317884469b395425e1edd4c857cce0afc80a68b6
Ei5enheim <rajesh@cs.unc.edu>
2024-02-19 13:07:36 -0500

    Install fzf tmux

--- install.zsh
+++ install.zsh
@@ -1,5 +1,9 @@
 # !/bin/zsh
 
+# download JetBrainsMono Nerd front from here
+# https://www.nerdfonts.com/font-downloads.
+# and copy to /Library/Fonts/
+
 # The absolute, canonical ( no ".." ) path to this script
 curr_dir=$(cd -P -- "$(dirname -- "$0")" && echo "$(pwd -P)")
 echo "source ${curr_dir}/setup.zsh" >> ~/.zshrc
@@ -10,8 +14,17 @@ brew install tmux
 # install fzf binary
 brew install fzf
 
+fzf_version=$(fzf --version | cut -d' ' -f1)
 # enable auto-complete
 $(brew --prefix)/opt/fzf/install
+
+tmux_file="/opt/homebrew/Cellar/fzf/${fzf_version}/bin/fzf-tmux"
+if [ -e "${tmux_file}" ]; then
+  rm -f /usr/local/bin/fzf-tmux
+  ln ${tmux_file} /usr/local/bin/fzf-tmux
+else
+    echo "${tmux_file} does not exist"
+fi
 
 # install fd
 # alternative to fd which is faster.
